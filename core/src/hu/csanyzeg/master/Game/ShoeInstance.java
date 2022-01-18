ac120613e507d7d0985d9479381239b27235f138
tuskeb <tuskebganz@gmail.com>
2022-01-18 17:14:20 +0100

    Cipőkolor

--- core/src/hu/csanyzeg/master/Game/ShoeInstance.java
+++ core/src/hu/csanyzeg/master/Game/ShoeInstance.java
@@ -1,8 +1,10 @@
 package hu.csanyzeg.master.Game;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.math.RandomXS128;
 
 public class ShoeInstance {
+    private static RandomXS128 random = new RandomXS128();
 
     public enum Cipohelye{
         JofogasonMegveheto, JofogasonMeghirdetettSzekrenybenlevo, SzekrenybenNemMeghirdetett
@@ -18,6 +20,6 @@ public class ShoeInstance {
         this.cipohelye = cipohelye;
         this.base = base;
         price = base.price;
-        color.rgb888(1f,1f,1f);
+        color.set(random.nextInt(4) * 0.3333f , random.nextInt(4) * 0.3333f, random.nextInt(4) * 0.3333f, 1f);
     }
 }
