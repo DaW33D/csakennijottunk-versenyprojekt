081c68cc88f9792559ed12c69e0ab43b0dd66d2e
Milaan1337 <71323999+Milaan1337@users.noreply.github.com>
2022-02-04 09:34:55 +0100

    Base

---
+++ core/src/csakennijottunk/Sources/LabelStyle.java
@@ -0,0 +1,11 @@
+package csakennijottunk.Sources;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+
+public class LabelStyle extends Label.LabelStyle{
+    public LabelStyle(BitmapFont bitmapFont, Color fontColor) {
+        super(bitmapFont, fontColor);
+    }
+}
